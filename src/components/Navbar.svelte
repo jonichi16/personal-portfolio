160fe921af8c226da3cbe6e52a82a2f54feafc84
Jhonel Villanueva <vvjhonel@gmail.com>
2023-06-04 04:39:25 +0800

    feat: Add cursor pointer to meny

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -20,7 +20,7 @@
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 24 24"
-				class="w-6 {hidden ? 'block' : 'hidden'} | md:hidden"
+				class="w-6 hover:cursor-pointer {hidden ? 'block' : 'hidden'} | md:hidden"
 				on:click={handleClick}
 				on:keydown={handleClick}
 				><title>menu</title><path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" /></svg
@@ -28,7 +28,7 @@
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 24 24"
-				class="w-6 {hidden ? 'hidden' : 'block'} | md:hidden"
+				class="w-6 hover:cursor-pointer {hidden ? 'hidden' : 'block'} | md:hidden"
 				on:click={handleClick}
 				on:keydown={handleClick}
 				><title>close</title><path
