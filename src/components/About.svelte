adfc1e4d8fc12babc4c2eeae731ba771daa52a63
Jhonel Villanueva <vvjhonel@gmail.com>
2023-06-04 00:16:28 +0800

    fix: Change id position to section

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -2,9 +2,9 @@
 	import island from '$lib/assets/images/island_illustration.svg'
 </script>
 
-<section class="flex justify-between items-center py-24">
+<section id="about" class="flex justify-between items-center py-24">
 	<img src={island} alt="Island" class="max-w-md w-5/12 hidden | sm:block" />
-	<div id="about" class="sm:pl-2">
+	<div class="sm:pl-2">
 		<h2 class="font-bold text-xl mb-3 | sm:text-4xl sm:text-center">About Me</h2>
 		<p class="text-sm max-w-md | sm:text-lg sm:text-center">
 			I am a passionate mechanical engineer with a strong desire to transition into a developer
--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -3,10 +3,8 @@
 	import ProjectsCard from './ProjectsCard.svelte'
 </script>
 
-<section class="py-24">
-	<h2 id="projects" class="font-bold text-xl mb-3 | sm:mb-4 sm:text-4xl sm:text-center">
-		Projects
-	</h2>
+<section id="projects" class="py-24">
+	<h2 class="font-bold text-xl mb-3 | sm:mb-4 sm:text-4xl sm:text-center">Projects</h2>
 	<ul class="grid gap-6 | md:grid-cols-2">
 		{#each projects as project (project.id)}
 			<li class="hover:scale-105">
